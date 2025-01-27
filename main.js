75f6fcb21cfc7d752be984012c4ddcd271b3f44c
sskrlo <afgwfawsd@gmail.com>
2025-01-27 04:50:08 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let typed = new Typed(".text" ,{
-
-  strings: ["Multi-Millionare", "designer lover","Multi-Millionare 2x","Looksmaxxer"],
-  typeSpeed: 50,
-  backSpeed: 80,
-  backDelay: 2000,
-  loop: true
-
-});
